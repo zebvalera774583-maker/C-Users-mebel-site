7da567e158357ffaad5ed8b503949e19efe99310
zebvalera774583-maker <zebvalera774583@gmail.com>
2025-12-22 21:40:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ashot Zebelyan",
+  description: "Мебель на заказ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
 
