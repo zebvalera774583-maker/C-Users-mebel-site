570c8f69cb84997bc485dc7ac91f1c1301ece5b5
zebvalera774583-maker <zebvalera774583@gmail.com>
2025-12-24 17:48:58 +0300

    Add files via upload

---
+++ layout.jsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ashot Zebelyan",
+  description: "Мебель на заказ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
